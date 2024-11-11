df137b17ce048d93b98a784ddb3acb37ae8d9a3b
Ali Fathi <alifathi1388@hotmail.com>
2024-11-11 05:25:36 +0330

    read "listening port" from env.

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import { indexRouter, taskRouter } from './routes';
 
 const app = express();
 
-app.set('port', 3000);
+app.set('port', parseInt(process.env.PORT) || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.disable('view cache');
